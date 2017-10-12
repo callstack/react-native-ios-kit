d2a683cbf975626b9c875fb0040e3c1bc9706053
lukewalczak <lukasz.walczak@callstack.io>
2017-10-12 15:45:19 +0200

    Fix flowtype

--- src/components/Typography/createText.js
+++ src/components/Typography/createText.js
@@ -4,14 +4,14 @@
 import React from 'react';
 import StyledText from './StyledText';
 
-export type TConfig = {
+type Config = {
   fontSize: number,
   tracking: number,
   leading: number,
   fontWeight: string,
 };
 
-const createText = (config: TConfig) => {
+const createText = (config: Config) => {
   return (props: Object) => <StyledText {...props} config={config} />;
 };
 
