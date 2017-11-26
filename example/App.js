c03d933ccc27b2fdd4c7582a9e14d437fb6be9f1
Adam <trzcinski.adam@gmail.com>
2017-11-26 17:54:42 +0100

    Added ThemeSelector screen to examples.  Some refactoring of ExamplList

--- example/App.js
+++ example/App.js
@@ -1,20 +1,62 @@
+/* @flow */
 import React from 'react';
 import { StyleSheet, NavigatorIOS } from 'react-native';
-import { ThemeProvider } from 'react-native-ios-kit';
+import { ThemeProvider, DefaultTheme } from 'react-native-ios-kit';
+import type { Theme } from 'react-native-ios-kit';
 
 import ExampleList from './src/ExampleList';
+import ThemeSelector from './src/ThemeSelector';
 
-export default class App extends React.Component {
+type Props = {
+  navigator: Object,
+};
+
+type State = {
+  selectedTheme: Theme,
+};
+
+export default class App extends React.Component<Props, State> {
+  state = {
+    selectedTheme: DefaultTheme,
+  };
+  _nav: ?Object;
+  openThemesScreen = () => {
+    if (this._nav) {
+      this._nav.push({
+        component: ThemeSelector,
+        title: 'Select Theme',
+        passProps: {
+          selectTheme: this.selectTheme,
+          selectedTheme: this.state.selectedTheme,
+        },
+      });
+    }
+  };
+
+  selectTheme = (theme: Object) => this.setState({ selectedTheme: theme });
+
   render() {
+    const { selectedTheme } = this.state;
+    const { barColor, buttonColor, text, phoneFieldBackground } = selectedTheme;
     return (
-      <ThemeProvider>
+      <ThemeProvider theme={selectedTheme}>
         <NavigatorIOS
+          ref={ref => (this._nav = ref)}
           initialRoute={{
             component: ExampleList,
             title: 'Example',
+            rightButtonTitle: 'Theme',
+            onRightButtonPress: this.openThemesScreen,
+            passProps: { theme: selectedTheme },
           }}
           style={styles.container}
-          itemWrapperStyle={styles.scene}
+          itemWrapperStyle={[
+            styles.scene,
+            { backgroundColor: phoneFieldBackground },
+          ]}
+          barTintColor={barColor}
+          tintColor={buttonColor}
+          titleTextColor={text}
         />
       </ThemeProvider>
     );
---
+++ example/src/ExampleListItem.js
@@ -0,0 +1,38 @@
+/* flow */
+import * as React from 'react';
+import { TouchableHighlight, StyleSheet, Text } from 'react-native';
+import { withTheme } from 'react-native-ios-kit';
+import type { Theme } from 'react-native-ios-kit';
+
+type Props = {
+  theme: Theme,
+  onPressRow: (rowData: Route) => void,
+  rowData: Route,
+};
+
+class ExampleLitItem extends React.PureComponent<Props> {
+  render() {
+    const { onPressRow, rowData, theme: { text } } = this.props;
+
+    return (
+      <TouchableHighlight
+        onPress={() => onPressRow(rowData)}
+        style={styles.row}
+        underlayColor="#C8C7CC"
+      >
+        <Text style={[styles.text, { color: text }]}>{rowData.title}</Text>
+      </TouchableHighlight>
+    );
+  }
+}
+
+export default withTheme(ExampleLitItem);
+
+const styles = StyleSheet.create({
+  row: {
+    padding: 15,
+  },
+  text: {
+    fontSize: 17,
+  },
+});
