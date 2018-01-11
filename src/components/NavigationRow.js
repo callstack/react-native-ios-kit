aaf6a322c4961be1f3ca9c8c57dd99e92fd9c26b
Adam Trzciński <trzcinski.adam@gmail.com>
2018-01-11 09:56:01 +0100

    NavigationRow component

---
+++ src/components/NavigationRow.js
@@ -0,0 +1,49 @@
+/* @flow */
+import * as React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import RowItem from './RowItem';
+import Icon from './Icon';
+import withTheme from '../core/withTheme';
+import { Body } from './Typography';
+import type { Theme } from '../types/Theme';
+
+type Props = {
+  theme: Theme,
+  onPress?: void => void,
+  info?: string,
+};
+
+class NavigationRow extends React.Component<Props> {
+  renderRightComponent = () => {
+    const { info, theme: { placeholderColor } } = this.props;
+    return (
+      <View style={styles.row}>
+        {info ? (
+          <Body style={{ color: placeholderColor, paddingRight: 10 }}>
+            {info}
+          </Body>
+        ) : null}
+        <Icon name="ios-arrow-forward" size={22} color={placeholderColor} />
+      </View>
+    );
+  };
+  render() {
+    const { onPress } = this.props;
+    return (
+      <RowItem
+        onPress={onPress}
+        rightComponent={this.renderRightComponent()}
+        {...this.props}
+      />
+    );
+  }
+}
+
+export default withTheme(NavigationRow);
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+  },
+});
